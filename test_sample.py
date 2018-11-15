031c9cbb313718bd764c7f33f29aa727c566f422
Victor Chudnovsky <vchudnov@google.com>
2018-11-14 16:10:10 -0800

    Read config and test files
    - config files listed on the command line are read and executed (though they cannot be used to set the environment yet)
    - test spec files listed on the command lines are read and run

--- test_sample.py
+++ test_sample.py
@@ -5,21 +5,23 @@
 # https://docs.python.org/3/library/functions.html#exec
 # https://pyyaml.org/wiki/PyYAMLDocumentation
 
-import yaml
+import logging
+import os.path
 import string
+import sys
 import testcase
-
-
+import yaml
 
 def main():
-  with open("test_spec.yaml", 'r') as stream:
-    try:
-        spec = yaml.load(stream)
-    except yaml.YAMLError as exc:
-        print(exc)
+  logging.basicConfig(level=logging.INFO)
+  logging.info("argv: {}".format(sys.argv))
+
+  config_files, test_files = read_args(sys.argv)
+  environments = create_environments(config_files)
+  test_suites = gather_test_suites(test_files)
 
   run_passed = True
-  for suite_num, suite in enumerate(spec["test"]["suites"]):
+  for suite_num, suite in enumerate(test_suites):
     if not suite.get("enabled", True):
       continue
     setup = suite.get("setup", "")
@@ -38,6 +40,39 @@ def main():
     exit(-1)
 
 
+def read_args(argv):
+  config_files = []
+  test_files = []
+  for filename in argv[1:]:
+    ext = os.path.splitext(filename)[-1]
+    if ext == ".py":
+      config_files.append(filename)
+    elif ext == ".yaml":
+      test_files.append(filename)
+    else:
+      msg = 'unknown file type: "{}"'.format(filename)
+      logging.critical(msg)
+      raise ValueError(msg)
+  return config_files, test_files
+
+# TODO(vchudnov): Move this to an "environment" module
+def create_environments(config_files):
+  environments = []
+  for filename in config_files:
+    logging.info('Reading config file "{}"'.format(filename))
+    # TODO(vchudnov) Pass the `register_test_environment()` function as a local
+    # symbol, and have that function modify `environments`
+    exec(open(filename).read())
+  return environments
+
+def gather_test_suites(test_files):
+  suites = []
+  for filename in test_files:
+    logging.info('Reading test file "{}"'.format(filename))
+    with open(filename, 'r') as stream:
+      spec = yaml.load(stream)
+      suites.extend(spec["test"]["suites"])
+  return suites
 
 
 #  eval(spec["test"]["case"])
