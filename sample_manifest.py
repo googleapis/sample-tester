d4f794864ffa81168854371411003b7e56dccfdb
Victor Chudnovsky <vchudnov@google.com>
2019-01-12 15:15:40 -0800

    Make a better manifesto class

--- sample_manifest.py
+++ sample_manifest.py
@@ -6,51 +6,59 @@ import yaml
 
 class Manifest:
   """Maintains a manifest of auto-generated sample artifacts."""
+  VERSION_KEY = "version"
+  SETS_KEY = "_sets"
+  ELEMENTS_KEY = "_items"
 
-  def __init__(self):
+
+  def __init__(self, *indices: str):
     self.interpreter = {"1": self.parse_manifest_v1}
 
-    # tags[language][label_type][label_value] = [metadata, metadata, ...]
-    # eg tags["python"]["path"]["/tmp/artifact"] = [ artifact1meta ]
-    # eg tags["python"]["region_tag"]["analyze_sentiment"] = [ sentiment_john_meta, sentiment_mary_meta ]
+    # tags[key1][key2]...[keyn] == [metadata, metadata, ...]
+    # eg with self.indices == ["language", "region_tag"]:
+    #    tags["python"]["analyze_sentiment"] = [ sentiment_john_meta, sentiment_mary_meta ]
     self.tags = {}
 
+    self.indices = indices or [None]
 
   def read_files(self, *files: str):
-    """ Reads sample manifests from files."""
-    return self.read_sources([(name, lambda: open(name, 'r')) for name in files])
+    self.read_sources(files_to_yaml(*files))
 
   def read_strings(self, *sources: str):
-    """ Reads sample manifests from strings."""
-    return self.read_sources([("string-{}".format(idx), lambda:io.StringIO(content)) for idx, content in enumerate(sources) if len(content) > 0])
+    self.read_sources(strings_to_yaml(*sources))
 
   def read_sources(self, sources):
-    """ Reads a sample manifest."""
+    """ Reads a sample manifest.
+
+    Args:
+      sources: An iterable of (name, manifest) pairs
+
+    Returns:
+      the list of sources successfully read
+    """
     err_no_version = []
     err_no_interpreter = []
     sources_read = []
-    for filename, opener in sources:
-      logging.info('Reading manifest "{}"'.format(filename))
-      with opener() as stream:
-        manifest = yaml.load(stream)
-        if not manifest:
-          continue
-        sources_read.append(filename)
+    for name, manifest in sources:
+      logging.info('reading manifest "{}"'.format(name))
+      if not manifest:
+        continue
+      sources_read.append(name)
 
-        version = manifest.get('version')
-        if version is None:
-          err_no_version.append(filename)
-          continue
-        interpreter = self.interpreter.get(str(version))
-        if not interpreter:
-          err_no_interpreter.append(filename)
-          continue
+      version = manifest.get(self.VERSION_KEY)
+      if version is None:
+        err_no_version.append(name)
+        continue
+      interpreter = self.interpreter.get(str(version))
+      if not interpreter:
+        err_no_interpreter.append(name)
+        continue
 
-        try:
-          interpreter(manifest)
-        except Exception as e:
-          logging.error('Error parsing manifest file "{}": {}'.format(filename, e))
-          raise
+      try:
+        interpreter(manifest)
+      except Exception as e:
+        logging.error('error parsing manifest file "{}": {}'.format(filename, e))
+        raise
 
     error = []
     if len(err_no_version) > 0:
@@ -64,33 +72,43 @@ class Manifest:
     return sources_read
 
   def parse_manifest_v1(self, input):
-    for sample_set in input.get("sample_sets"):
-      language = sample_set.get("language","")
-      base_dir = sample_set.get("base_dir", "")
-      language_manifest = get_or_create(self.tags, language, {})
+    max_idx = len(self.indices) - 1
 
-      all_samples = sample_set.get("samples", {})
-      for path, metadata in all_samples.items():
-        full_path = os.path.join(base_dir, path)
-        metadata["path"] = full_path
+    for sample_set in input.get(self.SETS_KEY):
 
-        for label_type, label_value in metadata.items():
-          type_manifest = get_or_create(language_manifest, label_type, {})
-          manifest_entry = get_or_create(type_manifest, label_value, [])
-          manifest_entry.append(metadata)
+      # get the tag defaults/prefixes for this set
+      set_values = sample_set.copy()
+      set_values.pop(self.ELEMENTS_KEY, None)
 
-        logging.info('read "{}"'.format(full_path))
+      all_elements = sample_set.get(self.ELEMENTS_KEY, [])
+      for element in all_elements:
+        # add the needed defaults/prefixes to this element
+        for key, value in set_values.items():
+          element[key] = value + element.get(key, "")
 
-  def get(self, language, label_type, label_value):
+        # store
+        tags = self.tags
+        for idx_num, idx_key in enumerate(self.indices):
+          idx_value = element.get(idx_key, "")
+          tags = get_or_create(tags,idx_value, [] if idx_num == max_idx else {})
+        tags.append(element)
+
+        logging.info('read "{}"'.format(element))
+
+  def get(self, *keys, **filters):
     """Returns the list of values associated with label_type, label_value"""
+    keys = keys or [None]
     try:
-      return self.tags[language][label_type][label_value]
+      tags = self.tags
+      for idx in range(0, len(self.indices)):
+        tags = tags[keys[idx]]
+      return[element for element in tags if all(tag_filter in element.items() for tag_filter in filters.items())]
     except Exception as e:
       return None
 
-  def get_one(self, language, label_type, label_value):
+  def get_one(self, *keys, **filters):
     """Returns the single value associated with label_type, label_value, or None otherwise"""
-    values = self.get(language, label_type, label_value)
+    values = self.get(*keys, **filters)
     if values is None or len(values) != 1:
       return None
     return values[0]
@@ -102,3 +120,17 @@ def get_or_create(d, key, empty_value):
     value = empty_value
     d[key] = value
   return value
+
+
+def files_to_yaml(*files: str):
+  """ Reads sample manifests from files."""
+  for name in files:
+    with open(name, 'r') as stream:
+      manifest = yaml.load(stream)
+    yield (name, manifest)
+
+def strings_to_yaml(*sources: str):
+  """ Reads sample manifests from strings."""
+  for data in sources:
+    manifest = yaml.load(data)
+    yield (name, manifest)
--- sample_manifest_test.py
+++ sample_manifest_test.py
@@ -2,142 +2,112 @@ import unittest
 import sample_manifest
 
 class TestManifest(unittest.TestCase):
-  def test_read_valid(self):
-    """This is a white-box test of the implementation.
-
-    There are black-box tests that test the input and output for these cases.
-    """
-
-    valid_manifest_file, expect_alice, expect_bob, expect_carol, expect_dan = self.get_valid_manifest()
-
-    manifest = sample_manifest.Manifest()
-    self.assertEqual(1, len(manifest.read_strings(valid_manifest_file)))
-
-    self.assertEqual(2, len(manifest.tags))
-    self.assertEqual(4, len(manifest.tags['python']))
-    self.assertEqual(2, len(manifest.tags['python']['region_tag']))
-    self.assertEqual(1, len(manifest.tags['python']['canonical']))
-    self.assertEqual(1, len(manifest.tags['python']['tag']))
-    self.assertEqual(2, len(manifest.tags['python']['path']))
-    self.assertEqual([expect_alice], manifest.tags['python']['region_tag']['alice'])
-    self.assertEqual([expect_alice], manifest.tags['python']['canonical']['trivial'])
-    self.assertEqual([expect_alice], manifest.tags['python']['path'][expect_alice['path']])
-    self.assertEqual([expect_bob], manifest.tags['python']['region_tag']['robert'])
-    self.assertEqual([expect_bob], manifest.tags['python']['tag']['guide'])
-    self.assertEqual([expect_bob], manifest.tags['python']['path'][expect_bob['path']])
-
-    self.assertEqual(2, len(manifest.tags['']))
-    self.assertEqual(1, len(manifest.tags['']['region_tag']))
-    self.assertEqual(2, len(manifest.tags['']['path']))
-    self.assertEqual([expect_carol], manifest.tags['']['path'][expect_carol['path']])
-    self.assertEqual([expect_dan], manifest.tags['']['path'][expect_dan['path']])
-    math = list(manifest.tags['']['region_tag']['math'])
-    for x in [expect_carol, expect_dan]:
-      math.remove(x)
-    self.assertEqual(0, len(math))
 
 
   def test_read_no_version(self):
-    manifest_file = (
-        'sample_sets:   \n'+
-        '- language: python   \n'+
-        '  base_dir : /home/nobody/api/samples   \n'+
-        '  samples :   \n'+
-        '    trivial/method/sample_alice:   \n'+
-        '      region_tag: alice   \n'+
-        '      canonical: trivial   \n')
-
-    manifest = sample_manifest.Manifest()
+    manifest_source = self.get_manifest_source()
+    manifest_source[0][1].pop('version')
+    manifest = sample_manifest.Manifest('language', 'region_tag')
     with self.assertRaises(Exception):
-      manifest.read_strings(manifest_file)
+      manifest.read_sources([manifest_source])
 
 
   def test_read_invalid_version(self):
-    manifest_file = (
-        'version: foo   \n'+
-        'sample_sets:   \n'+
-        '- language: python   \n'+
-        '  base_dir : /home/nobody/api/samples   \n'+
-        '  samples :   \n'+
-        '    trivial/method/sample_alice:   \n'+
-        '      region_tag: alice   \n'+
-        '      canonical: trivial   \n')
-
-    manifest = sample_manifest.Manifest()
+    manifest_source = self.get_manifest_source()
+    manifest_source[0][1]['version'] = "foo"
+    manifest = sample_manifest.Manifest('language', 'region_tag')
     with self.assertRaises(Exception):
-      manifest.read_strings(manifest_file)
+      manifest.read_sources(manifest_source)
 
   def test_get_(self):
-    valid_manifest_file, expect_alice, expect_bob, expect_carol, expect_dan = self.get_valid_manifest()
+    manifest_source, expect_alice, expect_bob, expect_carol, expect_dan = self.get_manifest_source()
 
-    manifest = sample_manifest.Manifest()
-    manifest.read_strings(valid_manifest_file)
+    manifest = sample_manifest.Manifest('language','region_tag')
+    manifest.read_sources([manifest_source])
 
-    self.assertEqual([expect_alice], manifest.get('python','region_tag','alice'))
-    self.assertEqual([expect_alice], manifest.get('python','canonical','trivial'))
-    self.assertEqual([expect_alice], manifest.get('python','path', expect_alice['path']))
+    self.assertEqual([expect_alice], manifest.get('python','alice'))
+    self.assertEqual([expect_alice], manifest.get('python','alice', canonical='trivial'))
+    self.assertEqual([expect_alice], manifest.get('python','alice', path=expect_alice['path']))
+    self.assertEqual([], manifest.get('python','alice', foo='bar'))
 
-    self.assertEqual([expect_bob], manifest.get('python','region_tag','robert'))
-    self.assertEqual([expect_bob], manifest.get('python','tag', 'guide'))
-    self.assertEqual([expect_bob], manifest.get('python','path', expect_bob['path']))
+    self.assertEqual([expect_bob], manifest.get('python','robert'))
+    self.assertEqual([expect_bob], manifest.get('python','robert', tag='guide'))
+    self.assertEqual([expect_bob], manifest.get('python','robert', path=expect_bob['path']))
 
-    self.assertEqual([expect_carol], manifest.get('','path', expect_carol['path']))
-    self.assertEqual([expect_dan], manifest.get('','path', expect_dan['path']))
+    self.assertEqual([expect_carol], manifest.get('','math', path=expect_carol['path']))
+    self.assertEqual([expect_dan], manifest.get('','math', path=expect_dan['path']))
 
-    self.assertIsNone(manifest.get('foo','region_tag','alice'))
+    self.assertIsNone(manifest.get('foo','alice'))
 
-    math = list(manifest.get('','region_tag','math'))
+    math = list(manifest.get('','math'))
     for x in [expect_carol, expect_dan]:
       math.remove(x)
     self.assertEqual(0, len(math))
 
   def test_get_one(self):
-    valid_manifest_file, expect_alice, expect_bob, expect_carol, expect_dan = self.get_valid_manifest()
+    manifest_source, expect_alice, expect_bob, expect_carol, expect_dan = self.get_manifest_source()
 
-    manifest = sample_manifest.Manifest()
-    manifest.read_strings(valid_manifest_file)
+    manifest = sample_manifest.Manifest('language', 'region_tag')
+    manifest.read_sources([manifest_source])
 
-    self.assertEqual(expect_alice, manifest.get_one('python','region_tag','alice'))
-    self.assertEqual(expect_alice, manifest.get_one('python','canonical','trivial'))
-    self.assertEqual(expect_alice, manifest.get_one('python','path', expect_alice['path']))
+    self.assertEqual(expect_alice, manifest.get_one('python','alice'))
+    self.assertEqual(expect_alice, manifest.get_one('python','alice', canonical='trivial'))
+    self.assertEqual(expect_alice, manifest.get_one('python','alice', path=expect_alice['path']))
+    self.assertIsNone(manifest.get_one('python','alice', foo='bar'))
 
-    self.assertEqual(expect_bob, manifest.get_one('python','region_tag','robert'))
-    self.assertEqual(expect_bob, manifest.get_one('python','tag', 'guide'))
-    self.assertEqual(expect_bob, manifest.get_one('python','path', expect_bob['path']))
+    self.assertEqual(expect_bob, manifest.get_one('python','robert'))
+    self.assertEqual(expect_bob, manifest.get_one('python','robert', tag='guide'))
+    self.assertEqual(expect_bob, manifest.get_one('python','robert', path=expect_bob['path']))
 
-    self.assertEqual(expect_carol, manifest.get_one('','path', expect_carol['path']))
-    self.assertEqual(expect_dan, manifest.get_one('','path', expect_dan['path']))
+    self.assertEqual(expect_carol, manifest.get_one('','math', path=expect_carol['path']))
+    self.assertEqual(expect_dan, manifest.get_one('','math', path=expect_dan['path']))
 
-    self.assertIsNone(manifest.get_one('foo','region_tag','alice'))
+    self.assertIsNone(manifest.get_one('foo','alice'))
 
-    self.assertIsNone(manifest.get_one('','region_tag','math'))
+    self.assertIsNone(manifest.get_one('','math'))
 
 
-  def get_valid_manifest(self):
-    valid_manifest_file = (
-        'version: 1   \n'+
-        'sample_sets:   \n'+
-        '- language: python   \n'+
-        '  base_dir : /home/nobody/api/samples   \n'+
-        '  samples :   \n'+
-        '    trivial/method/sample_alice:   \n'+
-        '      region_tag: alice   \n'+
-        '      canonical: trivial   \n'+
-        '    complex/method/usecase_bob:   \n'+
-        '      region_tag: robert   \n'+
-        '      tag: guide   \n'+
-        '- base_dir: /tmp   \n'+
-        '  samples:   \n'+
-        '    newer/carol:   \n'+
-        '      region_tag: math   \n'+
-        '    newest/dan:   \n'+
-        '      region_tag: math   \n')
+  def get_manifest_source(self):
+    manifest= {
+        sample_manifest.Manifest.VERSION_KEY: 1,
+        sample_manifest.Manifest.SETS_KEY: [
+            {
+                "language": "python",
+                "path": "/home/nobody/api/samples/",
+                sample_manifest.Manifest.ELEMENTS_KEY: [
+                    {
+                        "path": "trivial/method/sample_alice",
+                        "region_tag": "alice",
+                        "canonical": "trivial"
+                    },
+                    {
+                        "path": "complex/method/usecase_bob",
+                        "region_tag": "robert",
+                        "tag": "guide"
+                    }
+                ]
+            },
+            {
+                "path": "/tmp/",
+                sample_manifest.Manifest.ELEMENTS_KEY: [
+                    {
+                        "path": "newer/carol",
+                        "region_tag": "math"
+                    },
+                    {
+                        "path": "newest/dan",
+                        "region_tag": "math"
+                    }
+                ]
+            }
+        ]
+    }
 
-    expect_alice = { 'path': '/home/nobody/api/samples/trivial/method/sample_alice', 'region_tag': 'alice', 'canonical': 'trivial'}
-    expect_bob = { 'path': '/home/nobody/api/samples/complex/method/usecase_bob', 'region_tag': 'robert', 'tag': 'guide'}
+    expect_alice = { 'path': '/home/nobody/api/samples/trivial/method/sample_alice', 'language': 'python', 'region_tag': 'alice', 'canonical': 'trivial'}
+    expect_bob = { 'path': '/home/nobody/api/samples/complex/method/usecase_bob', 'language': 'python', 'region_tag': 'robert', 'tag': 'guide'}
     expect_carol = { 'path': '/tmp/newer/carol', 'region_tag': 'math'}
     expect_dan = { 'path': '/tmp/newest/dan', 'region_tag': 'math'}
-    return valid_manifest_file, expect_alice, expect_bob, expect_carol, expect_dan
+    return ("valid_manifest", manifest), expect_alice, expect_bob, expect_carol, expect_dan
 
 
 if __name__ == '__main__':
