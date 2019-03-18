adf5764c0b0624ea0e47de45da1f652922669e8f
Rebecca Taylor <rebeccataylor@google.com>
2019-03-18 13:58:39 -0700

    Fix devcheck for POSIX, readability (#12)
    * Fix devcheck for POSIX
    
    Also refactor for readibility and simplicity
    
    * Add --xunit test
    
    * Consistent #!
    
    * Further prettification
    
    * Perfectionistic consistency

---
+++ examples/convention-cloud/test-xunit-output.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "Test sample-tester --xunit=file output"
+
+xunit_file="$( mktemp /tmp/sample-tester.XXXXXXXXX )"
+
+sample-tester --xunit="$xunit_file" --convention=cloud examples/convention-cloud/language.test.yaml examples/mock-samples/googleapis
+
+xunit_xml="$( cat "$xunit_file" )"
+
+for expected_text in                        \
+  '<testsuite name="Language samples test'  \
+  '<testcase name="Sentiment from text'     \
+  '<system-out>'                            \
+  'In setup &quot;hi&quot'                  \
+  'Calling: python3 examples/mock-samples/' \
+  'In teardown bye'                         \
+;
+do
+  if ! grep --silent "$expected_text" "$xunit_file"; then
+    echo "Assertion failed"
+    echo "Expected $expected_text in:"
+    echo "$xunit_xml"
+    exit 1
+  fi
+done
+
+echo "Tests passed"
