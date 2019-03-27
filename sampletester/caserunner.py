9047aa93e155821a112a03024bd46a75aa118e36
Victor Chudnovsky <vchudnov@google.com>
2019-03-26 17:18:16 -0700

    In testplan files, s/target:/sample:/ (#35)
    This addresses partially #34 by hardcoding the change. In a future PR, we'll make this configurable.

--- sampletester/caserunner.py
+++ sampletester/caserunner.py
@@ -420,7 +420,7 @@ class TestCase:
     return parts[key_variable], parts[key_what]
 
   def params_for_call(self, parts):
-    key_cmd = "target"
+    key_cmd = "sample"
     key_params = "params"
     key_args = "args"
     if len(parts) < 1 or not key_cmd in parts:
