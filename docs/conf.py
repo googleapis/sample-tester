fe239dc748bbc34605aa6a8504db8e06fb0217ec
Victor Chudnovsky <vchudnov@google.com>
2019-04-18 16:25:39 -0700

    Tagging 0.11.0
    Add tag inclusion, argument substitution, chdir

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Google LLC'
 author = 'Victor Chudnovsky'
 
 # The short X.Y version
-version = '0.10.0'
+version = '0.11.0'
 # The full version, including alpha/beta/rc tags
-release = '0.10.0'
+release = '0.11.0'
 
 
 # -- General configuration ---------------------------------------------------
