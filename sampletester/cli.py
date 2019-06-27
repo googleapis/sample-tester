3d8bfadda2d1b00c2141a666618615f188e7f7e2
Victor Chudnovsky <vchudnov@google.com>
2019-06-27 15:30:30 -0700

    Tagging 0.15.1 (#95)
    Setup failures preempt test cases; doc fixes; tag interpolation in log statements. gen-manifest: quote YAML values, default to schema v3

--- sampletester/cli.py
+++ sampletester/cli.py
@@ -45,7 +45,7 @@ from sampletester import summary
 from sampletester import testplan
 from sampletester import xunit
 
-VERSION = '0.15.0'
+VERSION = '0.15.1'
 EXITCODE_SUCCESS = 0
 EXITCODE_TEST_FAILURE = 1
 EXITCODE_FLAG_ERROR = 2
